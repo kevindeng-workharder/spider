054989d3eb8be13a1a4696388a2baeb2702edb41
kevindeng-workharder <dengkaiwen2017@outlook.com>
2020-05-25 16:57:26 +0800

    test

--- spider.py
+++ spider.py
@@ -1,1 +1,7 @@
-print("hello")
+
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup as bf
+html = urlopen("http://www.baidu.com/")
+obj = bf(html.read(),'html.parser')
+title = obj.head.title
+print(title)
